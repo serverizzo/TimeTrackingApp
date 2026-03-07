c1aa89404bb780233fc2301fe4a7578deb16fe26
Edward Cerverizzo <edward.cerverizzo@gmail.com>
2026-03-06 19:27:00 -0600

    basic formatting for lap display

--- src/renderer/src/components/mainroute/lapdisplay/lapdisplay.tsx
+++ src/renderer/src/components/mainroute/lapdisplay/lapdisplay.tsx
@@ -1,5 +1,29 @@
+import { useStopwatch } from '@renderer/context/stopwatchcontext'
 import React from 'react'
 
 export default function LapDisplay() {
-  return <div>LapDisplay</div>
+  const { laps, convertToTime, millisecondsToTime } = useStopwatch()
+  return (
+    <div>
+      <div style={{ display: 'flex' }}>
+        <p style={styles.cellStyle}>lapTime</p>
+        <p style={styles.cellStyle}>cumulativeTotal</p>
+      </div>
+      {laps.map((ele) => (
+        <div style={{ display: 'flex' }}>
+          <p style={styles.cellStyle}>{convertToTime(millisecondsToTime(ele.lapTime))}</p>
+          <p style={styles.cellStyle}>{convertToTime(millisecondsToTime(ele.cumulativeTotal))}</p>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+const styles: { [key: string]: React.CSSProperties } = {
+  cellStyle: {
+    borderColor: 'green',
+    borderStyle: 'solid',
+    borderWidth: '1px',
+    margin: 5
+  }
 }
