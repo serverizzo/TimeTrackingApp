ea969dae422fc911f0025a40ebba12a980f036f3
Edward Cerverizzo <edward.cerverizzo@gmail.com>
2026-05-02 16:07:17 -0500

    feat: updated scrollbar design

--- src/renderer/src/components/visualizations/NotesPannel.tsx
+++ src/renderer/src/components/visualizations/NotesPannel.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import ReactMarkdown from 'react-markdown'
 
 export default function NotesPannel() {
   const [notes, setNotes] = useState<string>()
@@ -11,19 +12,37 @@ export default function NotesPannel() {
   }, [])
 
   return (
-    <div style={{ flex: 1, padding: '1rem' }}>
+    <div>
+      <style>
+        {`
+          .scroll_enabled {
+              overflow-y: scroll;
+              height: 500px;
+            }
+
+            .scroll_enabled::-webkit-scrollbar {
+              width: 6px;
+            }
+
+            .scroll_enabled::-webkit-scrollbar-track {
+              background: transparent;
+            }
+
+            .scroll_enabled::-webkit-scrollbar-thumb {
+              background-color: #444;
+              border-radius: 6px;
+            }
+
+            .scroll_enabled::-webkit-scrollbar-thumb:hover {
+              background-color: #666;
+            }
+        `}
+      </style>
       <p style={{ fontSize: 13, color: 'grey', marginBottom: 8 }}>Journal</p>
-      <pre
-        style={{
-          whiteSpace: 'pre-wrap',
-          wordBreak: 'break-word',
-          fontSize: 13,
-          lineHeight: 1.7,
-          margin: 0
-        }}
-      >
-        {notes}
-      </pre>
+      {/* <pre className="scroll_enabled">{notes}</pre> */}
+      <div className="scroll_enabled">
+        <ReactMarkdown>{notes}</ReactMarkdown>
+      </div>
     </div>
   )
 }
