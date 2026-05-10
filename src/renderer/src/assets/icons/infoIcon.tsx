7d6795b8ea0e66fac4778ff1df80c3570aaf4721
Edward Cerverizzo <edward.cerverizzo@gmail.com>
2026-05-10 12:33:33 -0500

    chore: correcting typos to remove error messages

--- src/renderer/src/assets/icons/infoIcon.tsx
+++ src/renderer/src/assets/icons/infoIcon.tsx
@@ -9,17 +9,9 @@ export default function InfoIcon({ size }: { size: number }) {
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <circle cx="8" cy="8" r="7" stroke="#6B7280" stroke-width="1.5" />
+      <circle cx="8" cy="8" r="7" stroke="#6B7280" strokeWidth="1.5" />
       <circle cx="8" cy="5.5" r="1" fill="#6B7280" />
-      <line
-        x1="8"
-        y1="8"
-        x2="8"
-        y2="12"
-        stroke="#6B7280"
-        stroke-width="1.5"
-        stroke-linecap="round"
-      />
+      <line x1="8" y1="8" x2="8" y2="12" stroke="#6B7280" strokeWidth="1.5" strokeLinecap="round" />
     </svg>
   )
 }
