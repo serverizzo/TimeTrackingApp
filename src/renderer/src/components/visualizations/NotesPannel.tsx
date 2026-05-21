f053957fcc988c78f111d47511dd72b604f77b69
Edward Cerverizzo <edward.cerverizzo@gmail.com>
2026-05-21 16:48:30 -0500

    chore: cleaning up leftover console log

--- src/renderer/src/components/visualizations/NotesPannel.tsx
+++ src/renderer/src/components/visualizations/NotesPannel.tsx
@@ -8,7 +8,7 @@ export default function NotesPannel() {
 
   useEffect(() => {
     window.api.getNotes().then((res) => {
-      console.log(res)
+      // console.log(res)
       setNotes(res)
 
       requestAnimationFrame(() => {
