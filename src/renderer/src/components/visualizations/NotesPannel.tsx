646db05fe19e15093dd19f9c6110adca965dfaf2
Edward Cerverizzo <edward.cerverizzo@gmail.com>
2026-05-29 11:01:25 -0500

    feat: add tabs to note pannel view

--- src/renderer/src/components/visualizations/NotesPannel.tsx
+++ src/renderer/src/components/visualizations/NotesPannel.tsx
@@ -5,6 +5,7 @@ import remarkGfm from 'remark-gfm'
 export default function NotesPannel() {
   const [notes, setNotes] = useState<string>()
   const previewRef = useRef<HTMLDivElement>(null)
+  const [notePannelView, setNotePannelView] = useState<string>('Timeline')
 
   useEffect(() => {
     window.api.getNotes().then((res) => {
@@ -45,11 +46,51 @@ export default function NotesPannel() {
             }
         `}
       </style> */}
-      <p style={{ fontSize: 13, color: 'grey', marginBottom: 8 }}>Journal</p>
-      {/* <pre className="scroll_enabled">{notes}</pre> */}
-      <div ref={previewRef} className="scroll_enabled">
-        <ReactMarkdown remarkPlugins={[remarkGfm]}>{notes}</ReactMarkdown>
+      <div>
+        {/* Button tabs */}
+        <div style={{ display: 'flex', gap: 5 }}>
+          <div
+            onMouseEnter={(e) => (e.currentTarget.style.background = '#4a4a4a')}
+            onMouseLeave={(e) => (e.currentTarget.style.background = '#383838')}
+            style={{ ...styles.tabStyles }}
+            onClick={() => setNotePannelView('Timeline')}
+          >
+            <p>Timeline</p>
+          </div>
+          <div
+            onMouseEnter={(e) => (e.currentTarget.style.background = '#4a4a4a')}
+            onMouseLeave={(e) => (e.currentTarget.style.background = '#383838')}
+            style={{ ...styles.tabStyles }}
+            onClick={() => setNotePannelView('Journal')}
+          >
+            <p>Journal</p>
+          </div>
+        </div>
+
+        {notePannelView === 'Timeline' && (
+          <div>
+            <p>TimelineView</p>
+          </div>
+        )}
+        {notePannelView === 'Journal' && (
+          <div ref={previewRef} className="scroll_enabled">
+            <ReactMarkdown remarkPlugins={[remarkGfm]}>{notes}</ReactMarkdown>
+          </div>
+        )}
       </div>
     </div>
   )
+}
+
+const styles: { [key: string]: React.CSSProperties } = {
+  tabStyles: {
+    // background:'white'
+    backgroundColor: '#383838',
+    cursor: 'pointer',
+    borderTopLeftRadius: 3,
+    borderTopRightRadius: 3,
+    padding: 5,
+    paddingLeft: 10,
+    paddingRight: 10
+  }
 }
