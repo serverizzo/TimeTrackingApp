544b8086c0e55198c597c4fbfb1d17114937dc16
Edward Cerverizzo <edward.cerverizzo@gmail.com>
2026-06-02 11:03:01 -0500

    style: style update

--- src/renderer/src/components/visualizations/NotesPannelSubComponents/Timeline.tsx
+++ src/renderer/src/components/visualizations/NotesPannelSubComponents/Timeline.tsx
@@ -29,41 +29,59 @@ export default function Timeline() {
         timelineData.map((entry) => (
           <div
             style={{
-              backgroundColor: '#888888',
+              backgroundColor: '#313131',
               marginBottom: 10,
               paddingLeft: 10,
               borderRadius: 6
               // padding: 10
             }}
           >
-            <h2>{formatDateWithDay(entry.date)}</h2>
+            <h2>
+              {formatDateWithDay(entry.date)}{' '}
+              {msToMins(
+                entry.laparray.reduce((cummulative, curr) => cummulative + curr.lap_time, 0)
+              )}
+            </h2>
             {/* daily checkins */}
-            {entry.activities?.map((activity) => (
-              <img
-                style={{ height: 30 }}
-                title={activity.icon_name}
-                src={
-                  activity.location ? `appicon://${userDataPath}/icons/${activity.location}` : ''
-                }
-              ></img>
-            ))}
+            <div
+              style={{
+                backgroundColor: '#535353',
+                display: 'flex',
+                padding: 8,
+                marginBottom: 5,
+                // marginTop: 5,
+                borderRadius: 6
+              }}
+            >
+              {entry.activities?.map((activity) => (
+                <img
+                  style={{ height: 30 }}
+                  title={activity.icon_name}
+                  src={
+                    activity.location ? `appicon://${userDataPath}/icons/${activity.location}` : ''
+                  }
+                ></img>
+              ))}
+            </div>
 
             {/* Laps */}
             {entry.laparray.map((lap) => {
               return (
                 <div style={{ display: 'flex', marginBottom: 5 }}>
                   <div
+                    title={lap.calendarname}
                     style={{
                       height: '50px',
                       width: '10px',
                       backgroundColor: lap.calendarcolor,
-                      marginRight: 10
+                      marginRight: 10,
+                      borderRadius: 4
                     }}
                   />
                   <div>
                     <p>{lap.note}</p>
                     <div style={{ marginLeft: 5 }}>
-                      <p>
+                      <p style={{ fontStyle: 'italic', color: '#7e7e7e' }}>
                         {lap.timestarted} - {lap.timestarted} ({msToMins(lap.lap_time)})
                       </p>
                     </div>
